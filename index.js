1be96f842e114408f00c1fe18da5101d6987c0c5
Yash Smart <yash.s4@ahduni.edu.in>
2024-03-04 13:32:44 +0530

    04-03-2024

--- index.js
+++ index.js
@@ -32,12 +32,16 @@ app.get('/register',(req,res) => {
 app.post('/ini',async (req,res) => {
     let Username = req.body.username;
     let Password = md5(req.body.password);
-    let data_obj = await db.query('select password from login_credentials where username = $1;',[Username])
+    let data_obj = await db.query('select id,password from login_credentials where username = $1;',[Username])
     // console.log(data_obj)
     if (data_obj.rows.length != 0) {
-        let data = data_obj.rows[0].password
+        let data = data_obj.rows[0].password;
+        let user_id = data_obj.rows[0].id;
+        console.log(user_id)
         if (data === Password) {
-            res.send('Hello ' + Username+ '!')
+            let quizzes_obj = await db.query('select * from quizzes where user_id = $1;',[user_id])
+            console.log(quizzes_obj.rows)
+            res.render('main.ejs',{data: quizzes_obj.rows,id: user_id})
         }
         else {
             res.send('Login failed')
@@ -56,6 +60,15 @@ app.post('/register',(req,res) => {
     res.redirect('/')
 })
 
+app.get('/add_quiz/:id',(req,res) => {
+    res.render('add quiz.ejs',{id:req.params.id})
+})
+
+app.post('/add_quiz/:id',async (req,res) => {
+    await db.query('Insert into quizzes(user_id,quiz_name,status) values($1,$2,0);',[req.params.id,req.body.quiz_name]);
+    let quizzes_obj = await db.query('select * from quizzes where user_id = $1;',[req.params.id])
+    res.render('main.ejs',{data: quizzes_obj.rows,id: req.params.id})
+})
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
